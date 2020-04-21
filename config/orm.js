97d0485667d64ccd946af911de488fd11f843380
Chris Ducharme <ducharme.christophert@gmail.com>
2020-04-21 15:45:21 -0500

    push

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,4 @@
-var connection = require("./connection.js");
-
+const connection = require("./connection");
 var orm = {
 	selectAll: function(tableName, callback) {
 		connection.query("SELECT * FROM ??", [tableName], function(err, result) {
@@ -8,24 +7,22 @@ var orm = {
 			}
 			callback(result);
 		});
-    },
-    
-    insertOne: function(tableName, objectToInsert, callback) {
-    connection.query("INSERT INTO ?? SET ?", [tableName, objectToInsert], function(err, result) {
-        if(err) {
-            throw err;
-        }
-        callback(result);
-         });
-    },
-    updateOne: function(tableName, itemToUpdate, conditon, callback) {
-    connection.query("UPDATE ?? SET ? WHERE ?", [tableName, itemToUpdate, conditon ], function(err, result) {
-        if(err) {
-            throw err;
-        }
-        callback(result);
-    });
-}
-    };
-
+	},
+	insertOne: function(tableName, objectToInsert, callback) {
+		connection.query("INSERT INTO ?? SET ?", [tableName, objectToInsert], function(err, result) {
+			if(err) {
+				throw err;
+			}
+			callback(result);
+		});
+	},
+	updateOne: function(tableName, itemToUpdate, condition, callback) {
+		connection.query("UPDATE ?? SET ? WHERE ?", [tableName, itemToUpdate, condition], function(err, result) {
+			if(err) {
+				throw err;
+			}
+			callback(result);
+		});
+	}
+};
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,41 +1,23 @@
-var express = require("express");
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 
-var PORT = process.env.PORT || 3306;
-
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection =mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'Ellycia93',
-    database: 'burgers_db'
-  });
-};
-
-
+let PORT = process.env.PORT || 8080;
 
 var app = express();
-
-
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-
-var exphbs = require("express-handlebars");
-
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
+const routes = require("./controllers/burgers_controller");
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function(){
+    console.log("Server listening at localhost:" + PORT);
 });
